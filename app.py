bbfd3dc6cc3a32e5ec73365c9b368e5e669d7c25
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2023-04-26 10:30:10 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
 import streamlit as st
 import pickle
+import string
+from heapq import nlargest
+import nltk
+from nltk.corpus import stopwords
+
+stop_words=stopwords.words("english")
+
+normalized_frequencies = pickle.load(open("normalized_frequencies.pkl", "rb"))
 
 
-normalized_frequencies=pickle.load(open("normalized_frequencies.pkl", "rb"))
+# --------------------------- Functions -----------------------
+def score_calculation(arr):
+    dicts = {}
+    for i in arr:
+        score = 0
+        holder = []
+        text = i.lower()
+        text = text.strip().split(" ")
+        for b in text:
+            if b not in stop_words and b not in string.punctuation:
+                a = "".join([j for j in b if j not in string.punctuation])
+                holder.append(a)
+
+        for j in holder:
+            if j in normalized_frequencies:
+                score += normalized_frequencies[j]
+
+        dicts[i] = score
+    return dicts
+
+
+def summary_generator(text):
+    text = text.strip().split(".")
+    d = score_calculation(text)
+    l = int(len(d) * 0.3)
+    res = nlargest(l, d, key=d.get)
+    return ". ".join(res)
+
+# --------------------------- UI --------------------------------
 st.title("SUMMARY GENERATOR")
